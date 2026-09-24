421606f21ddd64eb0d6fab7d016bd14f57dd8aaa
D-Sambath <donasambath75@gmail.com>
2026-09-24 21:14:01 +0700

    just Create Repo For Develop

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <h1 className="text-3xl text-amber-400  font-bold underline">
+        Hello World
+      </h1>
+    </>
+  )
+}
+
+export default App
